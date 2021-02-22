eccb5101c6ace50d3dbbd573576cdfb43902092d
icarusz <andrew.holz@gmail.com>
2021-02-22 13:30:18 -0500

    Default to dark skin to match our style.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,6 +32,37 @@ module.exports = {
             }, ],
             copyright: `Copyright © ${new Date().getFullYear()} Quorum Control GmbH.`,
         },
+        colorMode: {
+            // "light" | "dark"
+            defaultMode: "dark",
+      
+            disableSwitch: true,
+      
+            // Should we use the prefers-color-scheme media-query,
+            // using user system preferences, instead of the hardcoded defaultMode
+            respectPrefersColorScheme: false,
+      
+            // Dark/light switch icon options
+            switchConfig: {
+              // Icon for the switch while in dark mode
+              darkIcon: '🌙',
+      
+              // CSS to apply to dark icon,
+              // React inline style object
+              // see https://reactjs.org/docs/dom-elements.html#style
+              darkIconStyle: {
+                marginLeft: '2px',
+              },
+      
+              // Unicode icons such as '\u2600' will work
+              // Unicode with 5 chars require brackets: '\u{1F602}'
+              lightIcon: '\u{1F602}',
+      
+              lightIconStyle: {
+                marginLeft: '1px',
+              },
+            },
+        },      
     },
     presets: [
         [
