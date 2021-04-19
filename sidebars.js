135710f0db15315ac82c825f0be2f40a49e9e158
Topper Bowers <topper@toppingdesign.com>
2021-04-19 09:54:02 +0200

    move pieces around and add a liquidity rewards section

--- sidebars.js
+++ sidebars.js
@@ -1,8 +1,8 @@
 module.exports = {
     someSidebar: {
-        'Start here pleb': ['welcome','firstbet'],
+        'Start here pleb': ['welcome', 'firstbet'],
         'Collecting': ['collecting/gladiators'],
-        'Economy': ['economy/basics', 'economy/items', 'economy/tokenSupply'],
+        'Economy': ['economy/basics', 'economy/items', 'economy/tokenSupply', 'economy/liquidity'],
         'Game Mechanics': ['combat/introduction', 'combat/items', 'combat/combatexample'],
         'Tournaments': ['tournaments/introduction'],
         'Articles': [
